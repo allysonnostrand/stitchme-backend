f59a004f10720fccec5cf3a36cab5dc2e76a6238
iggy <allyson.nostrandd@gmail.com>
2022-05-31 14:59:59 -0700

    added boilerplate server code, server runs

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// const allRoutes = require('./controllers');
+const db = require('./config/connection');
+// const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// const { User } = require('./models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(cors())
+
+// app.use('/',allRoutes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
