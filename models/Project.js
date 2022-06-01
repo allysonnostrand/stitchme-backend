b8966d610ff6ce3a8a5018375fedbd7eb927abd4
iggy <allyson.nostrandd@gmail.com>
2022-05-31 19:53:08 -0700

    created project model

---
+++ models/Project.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const imageSchema = require('./Image')
+const todoSchema = require('/To-do')
+const stepSchema = require('/Step')
+
+const projectSchema = new Schema({
+    title: {type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 100
+    },
+
+    username: {type: String,
+        ref: 'user',
+        required: true
+    },
+
+    images: [imageSchema],
+
+
+    to_dos: [todoSchema],
+
+
+    steps: [stepSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+
+    id: false,
+});
+
+const Project = model('project', projectSchema);
+
+module.exports = Project;
