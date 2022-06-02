b74d0070312a68f28cd1cfd13a612ce2189cc4ca
iggy <allyson.nostrandd@gmail.com>
2022-06-02 14:46:40 -0700

    user can add a project now

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoute');
+const projectRoutes = require('./projectRoute');
 
 router.use('/users', userRoutes);
+router.use('/projects', projectRoutes);
 
 
 module.exports = router;
